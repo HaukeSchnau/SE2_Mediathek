17598a7ae569ef7c907b0276259da76f376589ad
Hauke Schnau <hauke@schnau-lilienthal.de>
2023-05-03 18:19:54 +0200

    Write Videospiel class (ex. 3.1.2)

---
+++ src/Videospiel.java
@@ -0,0 +1,31 @@
+public class Videospiel implements Medium {
+
+    private String _titel;
+    private String _kommentar;
+    private String _system;
+
+    public Videospiel(String titel, String kommentar, String system) {
+        _titel = titel;
+        _kommentar = kommentar;
+        _system = system;
+    }
+
+    @Override
+    public String getKommentar() {
+        return _kommentar;
+    }
+
+    @Override
+    public String getMedienBezeichnung() {
+        return "Videospiel";
+    }
+
+    @Override
+    public String getTitel() {
+        return _titel;
+    }
+
+    public String getSystem() {
+        return _system;
+    }
+}
