c3758489129fa3ae24d4d6ebf66337597b402b41
Hauke Schnau <hauke@schnau-lilienthal.de>
2023-05-03 18:19:29 +0200

    Write test class (ex. 3.1.1)

---
+++ src/VideospielTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VideospielTest {
+
+    private static final String KOMMENTAR = "Kommentar";
+
+    private static final String TITEL = "Titel";
+
+    private static final String SYSTEM = "System";
+
+    private static final String VIDEOSPIEL_BEZEICHNUNG = "Videospiel";
+
+    private Videospiel _videospiel1;
+
+    private Videospiel _videospiel2;
+
+    public VideospielTest() {
+        _videospiel1 = getMedium();
+        _videospiel2 = getMedium();
+    }
+
+    @Test
+    public void testGetMedienBezeichnung() {
+        String videospielBezeichnung = VIDEOSPIEL_BEZEICHNUNG;
+        assertEquals(videospielBezeichnung, _videospiel1.getMedienBezeichnung());
+    }
+
+    @Test
+    public void testKonstruktor() {
+        assertEquals(TITEL, _videospiel1.getTitel());
+        assertEquals(KOMMENTAR, _videospiel1.getKommentar());
+        assertEquals(SYSTEM, _videospiel1.getSystem());
+    }
+
+    @Test
+    /*
+     * Von ein und dem selben Videospiel kann es mehrere Exemplare geben, die von
+     * unterschiedlichen Personen ausgeliehen werden können.
+     */
+    public void testEquals() {
+        assertFalse("Mehrere Exemplare des gleichen Videospiels sollten ungleich sein",
+                _videospiel1.equals(_videospiel2));
+        assertTrue("Dasselbe Exemplare des gleichen Videospiels sollte gleich sein",
+                _videospiel1.equals(_videospiel2));
+    }
+
+    private Videospiel getMedium() {
+        return new Videospiel(TITEL, KOMMENTAR, SYSTEM);
+    }
+
+}
